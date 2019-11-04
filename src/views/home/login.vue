08c3f0e2aea43751a553786293387c18cccf32c2
liu-xtong <2576778991@qq.com>
2019-11-04 22:58:08 +0800

    tong

---
+++ src/views/home/login.vue
@@ -0,0 +1,161 @@
+<template>
+    <div class="login">
+        <router-view></router-view>
+       <div class="head">
+          <p>圈子登录</p>
+       </div>
+       <div class="main">
+          <input class="ipt1" type="text" placeholder="用户名" v-model="homes">
+          <input class="ipt2" type="mes" placeholder="密码" v-model="message">
+          <svg class="icon" aria-hidden="true">
+                <use xlink:href="icon-changyongicon"></use>
+          </svg>
+          <button>登录</button>
+          <router-link to="/register" tag="span">
+              没有账号，快速注册
+          </router-link>
+       </div>
+       <div class="footer">
+          <p class="p1">———————</p>
+          <p class="p2">第三方登录</p>
+          <p class="p3">———————</p>
+          <div class="box-tt">
+           <div class="tt">
+             <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-QQ"></use>
+            </svg>
+           </div>
+           <div class="tt">
+              <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-weixin"></use>
+            </svg>
+           </div>
+           <div class="tt">
+ <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-weibo"></use>
+            </svg>
+           </div>
+          </div>
+       </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'login',
+    props:{
+
+    },
+    components:{
+
+    },
+    data(){ 
+        return {
+          homes:'',
+          message:'',
+        }
+    },
+    computed:{
+
+    },
+    methods:{
+ 
+    },
+    created(){
+
+    },
+    mounted(){
+
+    }
+}
+</script>
+<style scoped>
+    .login{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+    .head{
+        width: 100%;
+        height: 50px;
+        background: #eee;
+        text-align: center;
+        line-height: 50px;
+    }
+    .head p{
+        font-size: 18px;
+    }
+    .main{
+        flex: 1;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+    }
+    .main .ipt1{
+        width: 90%;
+        height: 45px;
+        background: #eee;
+        border: none;
+        margin-left: 20px;
+        margin-top:30px;
+    }
+    .main .ipt2{
+        width: 90%;
+        height: 45px;
+        background: #eee;
+        border: none;
+        margin-left: 20px;
+        margin-top:20px;
+    }
+    .main span{
+        font-size: 15px;
+        margin-right: 240px;
+        margin-top:20px;
+        color: black;
+    }
+    button{
+        width: 90%;
+        height: 45px;
+        background: tomato;
+        border: none;
+        color: white;
+         margin-left: 20px;
+        margin-top:30px;
+    }
+    .footer{
+        width: 100%;
+        height: 100px;
+         font-size: 15px;
+    }
+    .footer .p1{
+        float: left;
+        margin-left: 55px;
+        color: #ccc;
+    }
+    .footer .p2{
+        float: left;
+        color: #ccc;
+    }
+    .p3{
+        color: #ccc;
+    }
+    .footer .box-tt{
+        width: 100%;
+        height: 80px;
+    }
+    .box-tt .tt{
+        width: 40px; 
+        height: 40px;
+        border-radius: 50%;
+        float: left;
+        margin-left: 70px;
+        margin-top:15px;
+    }
+    .icon {
+        width:40px;
+        height:40px;
+        vertical-align: -0.15em;
+        fill: currentColor;
+        overflow: hidden;
+        }
+</style>
---
+++ src/views/home/register.vue
@@ -0,0 +1,97 @@
+<template>
+    <div class="register">
+      <div class="head">
+          <router-link to="/login" tag="span" class="sp">
+             &lt;
+          </router-link>
+          <p>圈子登录</p>
+       </div>
+       <div class="main">
+          <input class="ipt1" type="text" placeholder="用户名">
+          <input class="ipt2" type="text" placeholder="密码">
+          <button>注册</button>
+       </div>
+       <div class="footer">
+         
+       </div>
+    </div>
+</template>
+<script>
+export default {
+    name:"register",
+    props:{
+
+    },
+    components:{
+
+    },
+    data(){
+        return {
+
+        }
+    },
+    computed:{
+
+    },
+    methods:{
+
+    },
+    created(){
+
+    },
+    mounted(){
+
+    }
+}
+</script>
+<style scoped>
+    .register{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+    .head{
+        width: 100%;
+        height: 50px;
+        background: #eee;
+        text-align: center;
+        line-height: 50px;
+    }
+    .head p{
+        font-size: 18px;
+        float: left;
+        margin-left: 140px;
+    }
+    .head .sp{
+        font-size: 15px;
+        float: left;
+        margin-left: 20px;
+    }
+    .main{
+        flex: 1;
+        text-align: center;
+    }
+    .main .ipt1{
+        width: 90%;
+        height: 45px;
+        background: #eee;
+        border: none;
+    }
+    .main .ipt2{
+        width: 90%;
+        height: 45px;
+        background: #eee;
+        border: none;
+    }
+    button{
+        width: 90%;
+        height: 45px;
+        background: tomato;
+        border: none;
+        color: white;
+    }
+    .footer span{
+         font-size: 15px;
+    }
+</style>
